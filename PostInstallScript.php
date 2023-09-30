dd9bcc669b4c671080fcae0d1702d09e10070c81
SteveAccount <boroczistvan0396@gmail.com>
2023-09-30 19:36:27 +0200

    2023-09-30

--- PostInstallScript.php
+++ PostInstallScript.php
@@ -4,6 +4,7 @@ namespace SteveEngine;
 
 class PostInstallScript {
     public static function script() {
-        file_put_contents("/log2.php", "Uff");
+        file_put_contents("log2.php", "Uff");
+        file_put_contents("log.php", "UffUff");
     }
 }
