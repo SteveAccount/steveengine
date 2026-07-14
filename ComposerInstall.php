712e3df5b32805d84425081e86dc0fef79a85bcb
SteveAccount <boroczistvan0396@gmail.com>
2026-07-14 13:40:52 +0200

    20260714

--- ComposerInstall.php
+++ ComposerInstall.php
@@ -2,14 +2,34 @@
 namespace SteveEngine;
 
 class ComposerInstall {
+    public array $folders = [
+        "Modules", "Main", "System"
+    ];
+
     public static function postInstall(\Composer\Script\Event $event) {
-        file_put_contents("hapci", "Hapcica");
-        mkdir("Kukucs");
+        $composerInstall = new self;
+        $composerInstall->setFolders();
     }
 
     public static function postUpdate(\Composer\Script\Event $event) {
-        file_put_contents("hapci", "Hapcica");
-        mkdir("Kukucs");
+        $composerInstall = new self;
+        $composerInstall->setFolders();
+    }
+
+    private function setFolders() {
+        $rootFolder =  realpath($_SERVER['DOCUMENT_ROOT']);
+        $appPath = dirname($rootFolder);
+
+        foreach ($this->folders as $folder) {
+            $modulesFolder = $appPath . DIRECTORY_SEPARATOR . $folder;
+            if (!is_dir($modulesFolder)) {
+                mkdir($modulesFolder);
+            }
+        }
+    }
+
+    private function setTables() {
+
     }
 }
 
